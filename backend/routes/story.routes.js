9bcbc16cf0c32b4faa09b87dac24f82a82946100
nitaidalal <dalalnitai7@gmail.com>
2026-02-26 02:07:26 +0530

    feat(story): add deleteStory route to allow users to delete their stories

--- backend/routes/story.routes.js
+++ backend/routes/story.routes.js
@@ -2,11 +2,12 @@ import express from "express";
 
 import authMiddleware from "../middlewares/auth.middleware.js";
 import {upload, handleMulterError} from "../middlewares/multer.middleware.js";
-import { getAllStory, getStoryByUsername, uploadStory } from "../controllers/story.controller.js";
+import { getAllStory, getStoryByUsername, uploadStory, deleteStory } from "../controllers/story.controller.js";
 
 const storyRouter = express.Router();
 
 storyRouter.post("/upload", authMiddleware, upload.single("media"), handleMulterError, uploadStory);
+storyRouter.delete("/delete/:storyId", authMiddleware, deleteStory);
 // storyRouter.get("/view/:storyId", authMiddleware, viewStory);
 storyRouter.get("/getByUsername/:username", authMiddleware, getStoryByUsername);
 storyRouter.get("/getAllStories", authMiddleware, getAllStory);
